0734a21488ad6fef74bcc5a6981b07c6c3753e86
Yves G <contact@webnumerik.fr>
2022-04-08 18:37:00 +0200

    test

--- src/components/AppartCard.test.js
+++ src/components/AppartCard.test.js
@@ -1,7 +1,6 @@
 import { render } from "@testing-library/react";
 import AppartCard from "./AppartCard";
 
-
 test('should render Card', () => { 
   render(<AppartCard><div id="test"></div></AppartCard>)
   const test = document.querySelector("#test")
